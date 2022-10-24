44662cf1207f2bb0b9d00285d2461f6566033c56
Anthony Guilherme <anthonytguilherme@hotmail.com>
2022-10-24 17:58:22 -0300

    bonus part in progress, validate the stickers

--- index.js
+++ index.js
@@ -2,15 +2,48 @@ const buttonSub = document.querySelector('.sub');
 const num = document.querySelector('.num');
 const buttonPlus = document.querySelector('.plus');
 
-buttonPlus.addEventListener('click', (event)=>{
+const msgError = document.querySelector('.msg-error')
+
+const span = document.querySelectorAll('.checkmark')
+
+const form = document.querySelector('.form');
+
+
+function validate() {
+  let valid = false;
+
+  if (document.getElementById('react').checked) {
+    valid = true;
+  } else if (document.getElementById('vue').checked) {
+    valid = true;
+  } else if (document.getElementById('angular').checked) {
+    valid = true;
+  }
+
+  if (valid) {
+    span.forEach((stikers)=>{
+      stikers.classList.remove('error')
+      msgError.style.display='none'
+    });
+  } else {
+    span.forEach((stikers) =>{
+      stikers.classList.add('error')
+    msgError.style.display='block'})
+      return false;
+  }
+}
+
+
+buttonPlus.addEventListener('click', () => {
   num.value = parseInt(num.value) + 1
 });
 
-buttonSub.addEventListener('click', (event)=>{
-  if(num.value <= 0){
+buttonSub.addEventListener('click', () => {
+  if (num.value <= 0) {
     num.value = 0
   }
-  else{
+  else {
     num.value = parseInt(num.value) - 1
   }
 });
+
