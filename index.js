604a053fd7061af6162b949a2699f62902f5eff2
Anthony Guilherme <anthonytguilherme@hotmail.com>
2022-10-24 16:33:33 -0300

    third part is completed with JS component

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("hello world");
+const buttonSub = document.querySelector('.sub');
+const num = document.querySelector('.num');
+const buttonPlus = document.querySelector('.plus');
+
+buttonPlus.addEventListener('click', (event)=>{
+  num.value = parseInt(num.value) + 1
+});
+
+buttonSub.addEventListener('click', (event)=>{
+  if(num.value <= 0){
+    num.value = 0
+  }
+  else{
+    num.value = parseInt(num.value) - 1
+  }
+});
